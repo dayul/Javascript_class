156d2ebb3e9bd6010e77f808fa804267ce04bf07
dayul <dyulchoo@naver.com>
2024-10-25 15:08:53 +0900

    1025 실습

---
+++ scripts/3-13(module).js
@@ -0,0 +1,18 @@
+// function log(message) {
+//     console.log(message);
+// }
+
+// function error(message) {
+//     console.error(message);
+// }
+
+export function log(message) {
+    console.log(message);
+}
+
+export function error(message) {
+    console.error(message);
+}
+
+// 상수만 가능
+export const PI = 3.14;
---
+++ scripts/3-13(ref).js
@@ -0,0 +1,5 @@
+import {log} from "/scripts/3-13(module).js"
+
+log("ref");
+
+// 전체 함수에서 필요로 하는 기능만 다시 모듈화
